34279ae5ff5185175bb2a2323be5a381fff9f506
songjiangtao <songjiangtao@ciicsh.com.pst>
2018-07-24 08:30:25 +0800

    昨天在上海没上传上来的mybatis自动生成文件

---
+++ mumschool-entity/src/main/java/com/sjt/cai/mumschool/entity/po/RelationBankQuestionPO.java
@@ -0,0 +1,79 @@
+package com.sjt.cai.mumschool.entity.po;
+
+import java.io.Serializable;
+
+import com.baomidou.mybatisplus.enums.IdType;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableName;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 宋江涛
+ * @since 2018-07-23
+ */
+@TableName("relation_bank_question")
+public class RelationBankQuestionPO extends Model<RelationBankQuestionPO> {
+
+    private static final long serialVersionUID = 1L;
+
+	@TableId(value="id", type= IdType.AUTO)
+	private Integer id;
+	@TableField("question_bank_id")
+	private Integer questionBankId;
+	@TableField("question_id")
+	private Integer questionId;
+	private Integer seq;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getQuestionBankId() {
+		return questionBankId;
+	}
+
+	public void setQuestionBankId(Integer questionBankId) {
+		this.questionBankId = questionBankId;
+	}
+
+	public Integer getQuestionId() {
+		return questionId;
+	}
+
+	public void setQuestionId(Integer questionId) {
+		this.questionId = questionId;
+	}
+
+	public Integer getSeq() {
+		return seq;
+	}
+
+	public void setSeq(Integer seq) {
+		this.seq = seq;
+	}
+
+	@Override
+	protected Serializable pkVal() {
+		return this.id;
+	}
+
+	@Override
+	public String toString() {
+		return "RelationBankQuestionPO{" +
+			", id=" + id +
+			", questionBankId=" + questionBankId +
+			", questionId=" + questionId +
+			", seq=" + seq +
+			"}";
+	}
+}
