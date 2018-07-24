5ea434e040af74c9ecd5b84c49f15aa79cedecd2
songjiangtao <songjtyg@sina.com>
2018-07-24 21:49:08 +0800

    昨晚在家没加进git的

---
+++ mumschool-dao/src/main/java/com/sjt/cai/mumschool/dao/RelationBankQuestionMapper.java
@@ -0,0 +1,16 @@
+package com.sjt.cai.mumschool.dao;
+
+import com.sjt.cai.mumschool.entity.po.RelationBankQuestionPO;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 宋江涛
+ * @since 2018-07-23
+ */
+public interface RelationBankQuestionMapper extends BaseMapper<RelationBankQuestionPO> {
+
+}
