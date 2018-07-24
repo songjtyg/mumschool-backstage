07c96d41795fc4fa5cc066410b3d8994632e1563
songjiangtao <songjiangtao@ciicsh.com.pst>
2018-07-24 08:46:51 +0800

    git 混乱了再次添加mabatis自动生成文件

---
+++ mumschool-dao/src/main/java/com/sjt/cai/mumschool/dao/QuestionBankMapper.java
@@ -0,0 +1,16 @@
+package com.sjt.cai.mumschool.dao;
+
+import com.sjt.cai.mumschool.entity.po.QuestionBank;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 宋江涛
+ * @since 2018-07-24
+ */
+public interface QuestionBankMapper extends BaseMapper<QuestionBank> {
+
+}
